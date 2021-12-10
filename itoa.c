10a6b51a2179177965ab549929998261cdf1544b
char-1ee <xingjianli59@gmail.com>
2021-12-10 14:29:55 +0800

    Update itoa.c

--- itoa.c
+++ itoa.c
@@ -5,13 +5,11 @@ void itoa(int n, char[] s) {
 
     if ((sign = n) < 0)
         n = -n;
-    
     do {
         s[i++] = n % 10 + '0';  // type conversion
     } while (n /= 10 > 0)
-
     if (sign < 0)
         s[i++] = '-';
-    s[i] = '\0';
+    s[i] = '\0';                // remeber '\0'
     reverse(s);
 }
