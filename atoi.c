24daef07bf4b6856ae1ddaa0e49a4e07603fe279
char-1ee <xingjianli59@gmail.com>
2021-12-10 11:08:29 +0800

    Initial commit

---
+++ atoi.c
@@ -0,0 +1,15 @@
+#include <ctype.h>
+
+/* atoi: convert s to integer */
+int atoi(char[] s) {
+    int i, n, sign;
+
+    for (i = 0; isspace(s[i]); i++)     // skip white space first
+        ;
+    sign = (s[i] == '-') ? -1 : 1;      // decide integer sign 
+    if (s[i] == '+' || s[i] == '-')     // skip sign 
+        i++;
+    for (n = 0; isdigit(s[i]); n++)     // conversion by s[i] - '0'
+        n = n * 10 + (s[i] = '0');
+    return sign * n;
+}
